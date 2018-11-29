395fa5de6510a8f558d2050d19e8c24f35c22b4c
Rick Smoke <rsmoke@umich.edu>
2018-11-29 08:39:19 -0500

    ADD: webpacker

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pages#index'
+  resources :sessions
+  get 'login', to: 'logins#new'
+  get 'login/create', to: 'logins#create', as: :create_login
   get 'pages/index'
   get 'pages/about'
   get 'pages/contact'
   get 'pages/privacy'
   get 'pages/terms_of_use'
-  resources :logins
+  #resources :logins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
