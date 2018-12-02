df189013dcd57ab543fb53d09af9937c4230a761
Rick Smoke <rsmoke@umich.edu>
2018-12-02 12:52:04 -0500

    MIGRATE: user schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_200738) do
+ActiveRecord::Schema.define(version: 2018_12_02_175020) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,17 @@ ActiveRecord::Schema.define(version: 2018_11_28_200738) do
     t.string "name", null: false
     t.string "google_id", null: false
     t.string "email_address", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "google_id"
+    t.string "name"
+    t.string "email_address"
+    t.string "avatar_url"
+    t.string "locale"
+    t.string "hosted_domain"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
