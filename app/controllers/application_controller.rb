b824f26d6d6ccd61c83c4c1db5eb8c33fba0feb3
Rick Smoke <rsmoke@umich.edu>
2019-02-05 09:36:55 -0500

    ADD: method that check for users payments existing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   include LoginsHelper
+
+private
+  def user_has_payments?(user)
+    # return true unless Payment.find_by(user_id: user).nil?
+    if Payment.find_by(user_id: user).nil?
+      false
+    else
+      true
+    end
+  end
+
+  helper_method :user_has_payments?
 end
