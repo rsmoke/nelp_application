8707d063aad417bcc20514eb5fe22f55662330a8
Rick Smoke <rsmoke@umich.edu>
2021-01-06 09:14:41 -0500

    cleanup alayout and flow

--- app/admin/payments.rb
+++ app/admin/payments.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Payment do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :transaction_type, :transaction_status, :transaction_id, :total_amount, :transaction_date, :account_type, :result_code, :result_message, :user_account, :payer_identity, :timestamp, :transaction_hash, :user_id
+  permit_params :transaction_type, :transaction_status, :transaction_id, :total_amount, :transaction_date, :account_type, :result_code, :result_message, :user_account, :payer_identity, :timestamp, :transaction_hash, :user_id
   #
   # or
   #
@@ -15,4 +15,23 @@ ActiveAdmin.register Payment do
   #   permitted
   # end
   
+  form do |f| # This is a formtastic form builder
+    f.semantic_errors # shows errors on :base
+    f.inputs do
+      f.input :user_id, as: :select, collection: User.all
+      f.input :total_amount
+      li "Transaction Type #{f.object.transaction_type}" unless f.object.new_record?
+      f.input :transaction_type, input_html: {value: "1"} unless f.object.persisted?
+      li "Transaction Status #{f.object.transaction_status}" unless f.object.new_record?
+      f.input :transaction_status, input_html: {value: "1"} unless f.object.persisted?
+      f.input :transaction_id
+      f.input :account_type
+      f.input :result_message
+      f.input :transaction_date, input_html: {value: "#{DateTime.now.strftime('%Y%m%d%H%M')}"}
+      # li "Camp Year #{f.object.camp_year}" unless f.object.new_record?
+      # f.input :camp_year, input_html: {value: current_camp_year} unless f.object.persisted?
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' button
+  end
+
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
 
   def index
+    if user_signed_in?
+      redirect_to all_payments_path
+    end
   end
 
   def about
